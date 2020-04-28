4c5270a8ea61814d9a77e65800d8ac148e43e7fd
ryan.w.daly <ryan.w.daly90@gmail.com>
2020-04-28 15:14:33 -0400

    added local server capability

--- public/js/app.js
+++ public/js/app.js
@@ -4,23 +4,19 @@
 */
 class TimersDashboard extends React.Component {
     state = {
-        timers: [
-            {
-                title: 'Practice squat',
-                project: 'Gym Chores',
-                id: uuid.v4(),
-                elapsed: 5456099,
-                runningSince: Date.now(),
-            },
-            {
-                title: 'Back squash',
-                project: 'Kitchen Chores',
-                id: uuid.v4(),
-                elapsed: 1273998,
-                runningSince: null,
-            },
-        ],  
+        timers: [],  
     };
+
+    componentDidMount() {
+        this.loadTimersFromServer();
+        setInterval(this.loadTimersFromServer, 5000);
+    }
+
+    loadTimersFromServer = () => {
+        client.getTimers((serverTimers) => {
+            this.setState({ timers: serverTimers })
+        })
+    }
 
     handleCreateFormSubmit = (timer) => {
         this.createTimer(timer);
@@ -48,6 +44,8 @@ class TimersDashboard extends React.Component {
         this.setState({
             timers: this.state.timers.concat(t),
         });
+
+        client.createTimer(t);
     };
 
     updateTimer = (attrs) => {
@@ -63,12 +61,18 @@ class TimersDashboard extends React.Component {
                 }
             })
         })
+
+        client.updateTimer(attrs);
     }
 
     deleteTimer = (timerId) => {
         this.setState({
             timers: this.state.timers.filter(t => t.id !== timerId)
         })
+
+        client.deleteTimer(
+            { id: timerId }
+        );
     }
 
     startTimer = (timerId) => {
@@ -85,6 +89,10 @@ class TimersDashboard extends React.Component {
                 }
             }),
         });
+
+        client.startTimer(
+            { id: timerId, start: now }
+        );
     };
 
     stopTimer = (timerId) => {
@@ -103,6 +111,10 @@ class TimersDashboard extends React.Component {
                 }
             }),
         });
+
+        client.stopTimer(
+            { id: timerId, stop: now }
+        );
     };
 
     render() {
